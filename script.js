4128784cf969784d8f50d2099745fc96c7d86ccc
Ryohei Mori <ryohei.mori0718@gmail.com>
2026-06-01 05:06:08 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+
+
+
+    function toggleMenu() {
+
+      const menu = document.getElementById("popupMenu");
+
+      menu.classList.toggle("active");
+
+    }
+
+    const menuButton = document.querySelector(".menu-button");
+
+    window.addEventListener("scroll", () => {
+
+      if (window.scrollY > 120) {
+
+        menuButton.classList.add("visible");
+
+      } else {
+
+        menuButton.classList.remove("visible");
+
+      }
+
+    });
+
+    function setLanguage(lang) {
+
+      const jp = document.querySelectorAll('.jp');
+      const en = document.querySelectorAll('.en');
+
+      if (lang === 'jp') {
+
+        jp.forEach(el => el.style.display = 'inline');
+        en.forEach(el => el.style.display = 'none');
+
+      } else {
+
+        jp.forEach(el => el.style.display = 'none');
+        en.forEach(el => el.style.display = 'inline');
+
+      }
+
+    }
+
+    setLanguage('en');
+    document.querySelectorAll('.gallery-scroll').forEach(gallery => {
+
+  gallery.addEventListener('wheel', (e) => {
+
+    e.preventDefault();
+
+    gallery.scrollLeft += e.deltaY;
+
+  });
+
+});
+
